b7bcc3d9f6fe648e9a193dc95e7ff7f66ec8900c
snnbyyds <snnbyyds@gmail.com>
2024-03-28 18:34:42 +0800

    lahaina: Build QTI USB service
    Change-Id: If73dd18c6e1edce309207a5c9e1b42c6a69f8e45

--- device.mk
+++ device.mk
@@ -285,6 +285,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service-qti
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
