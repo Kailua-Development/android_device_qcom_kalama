1c4f5d094d2c32c7a6a6f307b26081d6b32e887e
uwu-gl <moegluwu0@gmail.com>
2025-07-13 12:59:38 +0800

    kalama: vintf: Add audio manifest
    Change-Id: I6cb241c94ca8efd3676d3e1be88bdc4ffefa0c3a
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,7 +90,10 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MANIFEST_FILE += \
+    $(DEVICE_PATH)/configs/vintf/manifest.xml \
+    hardware/qcom-caf/sm8550/audio/primary-hal/configs/common/manifest_non_qmaa.xml \
+    hardware/qcom-caf/sm8550/audio/primary-hal/configs/common/manifest_non_qmaa_extn.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 # Init Boot
